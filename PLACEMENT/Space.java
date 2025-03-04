afee4aebbca0665c98900363f9bcd11e55110840
birju0451 <165183340+birju0451@users.noreply.github.com>
2025-03-04 14:36:29 +0530

    initial comment

---
+++ PLACEMENT/Space.java
@@ -0,0 +1,23 @@
+package PLACEMENT;
+
+public class Space {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        // System.out.println(Integer.BYTES); 
+        // System.out.println(Character.BYTES);
+
+        // double ans=0;
+        // System.out.println((ans));
+
+        int a=101+(0/3);
+        System.out.println(a);
+        double b=(3.0*'e')-(6*100000.1);
+        System.out.println(b);
+        boolean c=true && true;
+        System.out.println(c);
+        boolean d=false && true;
+        System.out.println(false && true);
+        System.out.println((false && false) || (true && true));
+
+    }
+}
---
+++ PLACEMENT/multiplication.java
@@ -0,0 +1,10 @@
+package PLACEMENT;
+
+public class multiplication {
+    public static void main(String[] args) {
+       
+        for (int i = 1; i <=10; i++) {
+            System.out.println(2 +" X "+ i +" = "+2*i);
+        }
+    }
+}
